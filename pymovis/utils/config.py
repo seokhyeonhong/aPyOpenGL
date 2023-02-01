fcaea4f972274b72405edda8f242e8eab264279f
SeokhyeonHong <37925049+SeokhyeonHong@users.noreply.github.com>
2023-02-01 22:45:06 +0900

    Update data preprocessing

--- pymovis/utils/config.py
+++ pymovis/utils/config.py
@@ -26,7 +26,6 @@ class DatasetConfig(Config):
         # filenames
         config.motion_pklname    = f"motion_length{config.window_length}_offset{config.window_offset}_fps{config.fps}.pkl"
         config.heightmap_pklname = f"heightmap_sparsity{config.sparsity}_mapsize{config.mapsize}.pkl"
-        config.envmap_pklname    = f"envmap_length{config.window_length}_offset{config.window_offset}_fps{config.fps}_sparsity{config.sparsity}_mapsize{config.mapsize}_topk{config.top_k}.pkl"
-        config.vis_pklname       = f"vis_length{config.window_length}_offset{config.window_offset}_fps{config.fps}_sparsity{config.sparsity}_mapsize{config.mapsize}_topk{config.top_k}.pkl"
+        config.env_pklname       = f"env_length{config.window_length}_offset{config.window_offset}_fps{config.fps}_sparsity{config.sparsity}_mapsize{config.mapsize}_topk{config.top_k}.pkl"
 
         return config
