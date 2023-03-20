38ddaeb7ba0445fcc69ae4a67fe33144f3390c77
SeokhyeonHong <37925049+SeokhyeonHong@users.noreply.github.com>
2023-03-20 20:54:58 +0900

    Fix fbx module not to be imported when there isn't

--- pymovis/motion/__init__.py
+++ pymovis/motion/__init__.py
@@ -1,3 +1,7 @@
 from pymovis.motion.core import Joint, Skeleton, Pose, Motion
 from pymovis.motion.data.bvh import BVH
-from pymovis.motion.data.fbx import FBX
+
+try:
+    from pymovis.motion.data.fbx import FBX
+except ImportError:
+    pass
