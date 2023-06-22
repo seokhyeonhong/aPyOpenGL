122dfed4f0ef591388c2afdfe41ddf7b22acd76c
SeokhyeonHong <37925049+SeokhyeonHong@users.noreply.github.com>
2023-06-22 22:56:36 +0900

    Update teapot model to be smaller

--- examples/vis_obj.py
+++ examples/vis_obj.py
@@ -5,7 +5,7 @@ from pymovis.vis import App, AppManager, Render
 class ObjApp(App):
     def __init__(self):
         super().__init__()
-        self.character = Render.obj("data/character.obj", scale=0.01).set_background(0.1)
+        self.character = Render.obj("data/teapot.obj", scale=0.01).set_background(0.1)
 
     def render(self):
         super().render()
