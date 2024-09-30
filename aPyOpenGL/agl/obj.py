bdb9de9a7707f58d84fe5426dba6ac7a3c2a2b89
seokhyeonhong <graphics.shong@gmail.com>
2024-09-30 19:40:34 +0900

    Fix OBJ triangulation and material

--- aPyOpenGL/agl/obj.py
+++ aPyOpenGL/agl/obj.py
@@ -7,13 +7,13 @@ import glm
 from .core import VAO, VertexGL, bind_mesh
 from .material import Material
 
-def parse_obj(path, scale):
+def parse_obj(path, scale, verbose=False):
     if not path.endswith(".obj"):
         raise Exception(f"File must be .obj format, but got {path}")
     
     positions, uvs, normals, faces = [], [], [], []
     mtl_files = []
-    current_mtl = None
+    current_mtl = "default"
 
     with open(path, "r") as f:
         lines = f.readlines()
@@ -36,7 +36,7 @@ def parse_obj(path, scale):
 
             # vertex
             elif prefix == "v":
-                vertex = glm.vec3(list(map(float, tokens[1:]))) * scale
+                vertex = glm.vec3(list(map(float, tokens[1:4]))) * scale
                 positions.append(vertex)
             
             # uv
@@ -51,33 +51,44 @@ def parse_obj(path, scale):
             
             # face
             elif prefix == "f":
-                if len(tokens[1:]) == 3:
-                    # exception for no material
-                    if current_mtl is None:
-                        raise Exception("Face must be preceded by a material")
-                    
-                    # parse vertices
-                    vertices = [tokens[1].split("/"), tokens[2].split("/"), tokens[3].split("/")]
+                vertices = [token.split("/") for token in tokens[1:]]
+                
+                if len(vertices) < 3:
+                    raise Exception(f"Faces with less than 3 vertices are not supported: {line}")
+                
+                # Triangulate if the face has more than 3 vertices
+                elif len(vertices) == 3:
+                    # Simple triangle, directly use it
                     for vtn in vertices:
                         position_index = int(vtn[0]) - 1
-                        uv_index       = int(vtn[1]) - 1 if len(vtn) > 1 and vtn[1] != "" else -1
-                        normal_index   = int(vtn[2]) - 1 if len(vtn) > 2 and vtn[2] != "" else -1
+                        uv_index = int(vtn[1]) - 1 if len(vtn) > 1 and vtn[1] != "" else -1
+                        normal_index = int(vtn[2]) - 1 if len(vtn) > 2 and vtn[2] != "" else -1
                         faces.append((position_index, uv_index, normal_index, current_mtl))
                 else:
-                    raise Exception(f"We only support faces with three vertices, but got {len(tokens[1:])} vertices: {line}")
-            
+                    # general fan triangulation, taking the first vertex as the anchor
+                    for i in range(1, len(vertices) - 1):
+                        triangle = [vertices[0], vertices[i], vertices[i + 1]]
+                        for vtn in triangle:
+                            position_index = int(vtn[0]) - 1
+                            uv_index = int(vtn[1]) - 1 if len(vtn) > 1 and vtn[1] != "" else -1
+                            normal_index = int(vtn[2]) - 1 if len(vtn) > 2 and vtn[2] != "" else -1
+                            faces.append((position_index, uv_index, normal_index, current_mtl))
             # material
             elif prefix == "usemtl":
                 current_mtl = tokens[1]
             
             # unknown
             else:
-                print("Line {}: Unknown line {}".format(line_idx, line.replace("\n", "")))
+                if verbose:
+                    print("Line {}: Unknown line {}".format(line_idx, line.replace("\n", "")))
                 continue
-
+                
     return positions, uvs, normals, faces, mtl_files
 
 def parse_mtl(path):
+    if path is None:
+        return {}
+    
     if not path.endswith(".mtl"):
         raise Exception(f"File must be .mtl format, but got {path}")
     
@@ -146,15 +157,16 @@ class Obj(VAO):
 
         self.materials = materials
     
+    
     @staticmethod
     def generate_vertices(path, scale=1.0):
         positions, uvs, normals, faces, mtl_files = parse_obj(path, scale)
 
         # parse materials
-        mtl_dict = {}
+        mtl_dict = { "default": Material() }
         for file in mtl_files:
-            mtl_dict.update(parse_mtl(file))
-        
+            if file is not None:
+                mtl_dict.update(parse_mtl(file))
         materials = [mtl_dict[name] for name in mtl_dict.keys()]
         name_to_mtl_idx = {name: idx for idx, name in enumerate(mtl_dict.keys())}
 
