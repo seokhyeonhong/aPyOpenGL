b4fb32738e7813aeb74aa843114bb6882387fe7d
seokhyeonhong <graphics.shong@gmail.com>
2024-10-07 22:37:43 +0900

    Update to support FbxSurfaceMaterial

--- aPyOpenGL/agl/fbxparser/material.py
+++ aPyOpenGL/agl/fbxparser/material.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import fbx
+import FbxCommon
 import glm
 
 from .parser import MaterialInfo
@@ -29,7 +30,58 @@ def get_materials(geometry) -> list[MaterialInfo]:
                 implementation_type = "CGFX"
 
             if not implementation:
-                if material.GetClassId().Is(fbx.FbxSurfacePhong.ClassId):
+                if material.GetClassId().Is(fbx.FbxSurfaceMaterial.ClassId):
+                    info.type = "default"
+
+                    # ambient
+                    ambient_prop = material.FindProperty(fbx.FbxSurfaceMaterial.sAmbient)
+                    if ambient_prop.IsValid():
+                        val = FbxCommon.FbxPropertyDouble3(ambient_prop).Get()
+                        color.Set(val[0], val[1], val[2])
+                        info.ambient = glm.vec3(color.mRed, color.mGreen, color.mBlue)
+
+                    # diffuse
+                    diffuse_prop = material.FindProperty(fbx.FbxSurfaceMaterial.sDiffuse)
+                    if diffuse_prop.IsValid():
+                        val = FbxCommon.FbxPropertyDouble3(diffuse_prop).Get()
+                        color.Set(val[0], val[1], val[2])
+                        info.diffuse = glm.vec3(color.mRed, color.mGreen, color.mBlue)
+                    
+                    # specular
+                    specular_prop = material.FindProperty(fbx.FbxSurfaceMaterial.sSpecular)
+                    if specular_prop.IsValid():
+                        val = FbxCommon.FbxPropertyDouble3(specular_prop).Get()
+                        color.Set(val[0], val[1], val[2])
+                        info.specular = glm.vec3(color.mRed, color.mGreen, color.mBlue)
+                        
+                    # emissive
+                    emissive_prop = material.FindProperty(fbx.FbxSurfaceMaterial.sEmissive)
+                    if emissive_prop.IsValid():
+                        val = FbxCommon.FbxPropertyDouble3(emissive_prop).Get()
+                        color.Set(val[0], val[1], val[2])
+                        info.emissive = glm.vec3(color.mRed, color.mGreen, color.mBlue)
+
+                    # opacity
+                    opacity_prop = material.FindProperty(fbx.FbxSurfaceMaterial.sTransparencyFactor)
+                    if opacity_prop.IsValid():
+                        val = FbxCommon.FbxPropertyDouble1(opacity_prop).Get()
+                        info.opacity = 1.0 - val
+
+                    # shininess
+                    shininess_prop = material.FindProperty(fbx.FbxSurfaceMaterial.sShininess)
+                    if shininess_prop.IsValid():
+                        val = FbxCommon.FbxPropertyDouble1(shininess_prop).Get()
+                        info.shininess = val
+
+                    # reflectivity
+                    reflectivity_prop = material.FindProperty(fbx.FbxSurfaceMaterial.sReflectionFactor)
+                    if reflectivity_prop.IsValid():
+                        val = FbxCommon.FbxPropertyDouble1(reflectivity_prop).Get()
+                        info.reflectivity = val
+
+                    materials.append(info)
+                    
+                elif material.GetClassId().Is(fbx.FbxSurfacePhong.ClassId):
                     info.type = "phong"
 
                     # ambient
@@ -65,6 +117,7 @@ def get_materials(geometry) -> list[MaterialInfo]:
                     info.reflectivity = val
 
                     materials.append(info)
+
                 elif material.GetClassId().Is(fbx.FbxSurfaceLambert.ClassId):
                     info.type = "lambert"
 
@@ -88,6 +141,7 @@ def get_materials(geometry) -> list[MaterialInfo]:
                     info.opacity = 1.0 - val
 
                     materials.append(info)
+
                 else:
                     print("Unknown material type: ", material.GetClassId().GetName())
 
